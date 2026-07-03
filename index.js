79d997bc6737e9d1546c4e1520edc1de7e55e475
“mateus” <mateus.bohn.tb@gmail.com>
2026-07-02 22:03:33 -0300

    iniciando  api com node express

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express"
+
+//server ou app
+
+const app = express()
+//Configura uma rota ´para o método HTTP GET na raiz ("/") do aplicativo.
+//path params - router 
+app.get('/api/v1/somar', (req, res) => {
+    // query params
+  const numero1 =Number(req.query.batata)
+  const numero2 =Number(req.query.numero2)
+
+const resultado = numero1 + numero2 
+res.send({ resultado })
+});
+
+
+
+// inicia o servidor express na porta 3000 e define uma função de retorno de uma chamada 
+app.listen(3000 , () => {
+    // Exibe uma mensagem no console quando quando o servidor estiver ativo .
+    console.log("Servidor ouvindo na porta 3000")
+})
